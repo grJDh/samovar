c7f323d12292ad249be5fe3419a57817d6fa1173
grJDh <rusmukhametov@gmail.com>
2020-05-30 18:51:36 +0300

    Updates

---
+++ precache-manifest.34950300e9dd8e46e43ecf7617db48cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3889ee29c6b1946a75e413e1ed649e55",
+    "url": "/samovar/index.html"
+  },
+  {
+    "revision": "33deeeb5b2dc01a7affa",
+    "url": "/samovar/static/css/main.836c92a1.chunk.css"
+  },
+  {
+    "revision": "c2837a370d2879394531",
+    "url": "/samovar/static/js/2.cc0fa581.chunk.js"
+  },
+  {
+    "revision": "80abf8651ad8beab3ba17d6b6d06b924",
+    "url": "/samovar/static/js/2.cc0fa581.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33deeeb5b2dc01a7affa",
+    "url": "/samovar/static/js/main.3f08ed7d.chunk.js"
+  },
+  {
+    "revision": "dc4442fa93bc912a4b72",
+    "url": "/samovar/static/js/runtime-main.d4a8d443.js"
+  }
+]);
